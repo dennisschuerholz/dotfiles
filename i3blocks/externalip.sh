70a55d832207235833e2290e92abac8e167ba4b1
Dennis Schürholz <deschuer@informatik.uni-bremen.de>
2017-02-21 02:07:36 +0100

    Better external ip i3block
    * my script now caches the retrieved address for one minute
    * the script toggles between v4 and v6 address

--- i3blocks/externalip.sh
+++ i3blocks/externalip.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 # shows external ip address and allow to copy it to the system clipboard using
-# the right and middle button of the mouse
+# the right and middle button of the mouse, also left click toggles between the
+# ip v4 and ip v6 addresses
 # adapted from Alexander Keller https://github.com/vivien/i3blocks/wiki/Writing-Your-Own-Blocklet#my-external-ip
 # 2017 Dennis Schürholz - https://dennisschuerholz.de
 
-IP_VERSION="v4"
-if [ $BLOCK_INSTANCE -eq 6 ]; then
-  IP_VERSION="v6"
+LOCKFILE="/tmp/externalip.v6.lock"
+if [[ $BLOCK_BUTTON -eq 1 ]]; then
+  if [ -f $LOCKFILE ]; then
+    rm $LOCKFILE
+  else
+    touch $LOCKFILE
+  fi
 fi
-IP=$(curl -m 10 https://$IP_VERSION.ifconfig.co)
-if [ -z $IP ]; then
-  echo "no public $IP_VERSION address"
+IP_VERSION=4
+if [ -f $LOCKFILE ]; then
+  IP_VERSION=6
+fi
+IP_FILE="/tmp/externalip.v$IP_VERSION.address"
+if [ -z `find $IP_FILE -mmin 1` ]; then
+  IP_ADDRESS=$(curl -m 10 https://v$IP_VERSION.ifconfig.co)
+  echo "$IP_ADDRESS">$IP_FILE
+else
+  IP_ADDRESS=`cat $IP_FILE`
+fi
+if [ -z $IP_ADDRESS ]; then
+  echo "no public v$IP_VERSION address"
   exit 33
 fi
 case $BLOCK_BUTTON in
-  [2-3]) echo -n "$IP" | xsel -b -i ;;
-  *) echo "$IP" ;;
+  [2-3]) echo -n "$IP_ADDRESS" | xsel -b -i ;;
+  *) ;;
 esac
+echo "$IP_ADDRESS"
